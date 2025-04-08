e6e80eb865c5097a7d522f9259347bde4262f2cd
Filipe Maciel <83095857+FilipeMaciel9000@users.noreply.github.com>
2025-04-08 18:12:55 -0300

    exercicio_3.py

--- exercicio_3.py
+++ exercicio_3.py
@@ -1,3 +1,4 @@
+# Crie um programa que leia dois números e mostre a soma entre eles.
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
 print(f'A soma dos dois números é: {n1 + n2}')
