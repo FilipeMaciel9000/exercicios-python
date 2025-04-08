bbc26b007cb4445f00dd9407850e4d74887d14be
Filipe Maciel <83095857+FilipeMaciel9000@users.noreply.github.com>
2025-04-08 18:00:13 -0300

    exercicio_6.py

--- exercicio_6.py
+++ exercicio_6.py
@@ -1,3 +1,4 @@
+# Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
 n = int(input('Digite um número: '))
 print(f'O dobro de {n} é: {n*2}')
 print(f'O triplo de {n}  é: {n*3}')
