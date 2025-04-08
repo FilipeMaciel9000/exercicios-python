532c25811251819efc65116f9c3f17c26c6dcbfd
Filipe Maciel <83095857+FilipeMaciel9000@users.noreply.github.com>
2025-04-08 18:02:07 -0300

    exercicio_9.py

--- exercicio_9.py
+++ exercicio_9.py
@@ -1,5 +1,18 @@
 #  Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
+# Solicita ao usuário o número para mostrar a tabuada
 n = int(input('Digite um número: '))
+
+# Exibe a mensagem inicial com o número escolhido
 print(f'Tabuada de {n}:')
-for i in range(1, 11):
-    print(f'{n} * {i} = {i*n}')
+
+# Imprime a tabuada manualmente
+print(f'{n} * 1 = {n * 1}')
+print(f'{n} * 2 = {n * 2}')
+print(f'{n} * 3 = {n * 3}')
+print(f'{n} * 4 = {n * 4}')
+print(f'{n} * 5 = {n * 5}')
+print(f'{n} * 6 = {n * 6}')
+print(f'{n} * 7 = {n * 7}')
+print(f'{n} * 8 = {n * 8}')
+print(f'{n} * 9 = {n * 9}')
+print(f'{n} * 10 = {n * 10}')
