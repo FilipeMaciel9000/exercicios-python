c9b906248aa18ca31c2946042e4c07801f63e0d6
Filipe Maciel <83095857+FilipeMaciel9000@users.noreply.github.com>
2025-04-08 17:59:02 -0300

    exercicio_7.py

--- exercicio_7.py
+++ exercicio_7.py
@@ -1,3 +1,4 @@
+#  Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
 print(f'A média dos desses números é: {(n1 + n2)/2}')
